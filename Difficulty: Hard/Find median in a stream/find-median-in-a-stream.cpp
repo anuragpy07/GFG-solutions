797d1fc0d7c03ec2c6c254e2543f0af4663f6dc2
Anurag Kumar <anuragpandey93345@gmail.com>
2024-11-15 23:24:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Find median in a stream/find-median-in-a-stream.cpp
@@ -0,0 +1,109 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+
+#include <iostream>
+#include <sstream>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <cctype>
+#include <string>
+#include <vector>
+#include <list>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <functional>
+using namespace std;
+
+#define DEBUG(x) cout << '>' << #x << ':' << x << endl;
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define FORD(i,a,b) for(int i=(a);i>=(b);i--)
+inline bool EQ(double a, double b) { return fabs(a-b) < 1e-9; }
+
+const int INF = 1<<29;
+typedef long long ll;
+
+inline int two(int n) { return 1 << n; }
+inline int test(int n, int b) { return (n>>b)&1; }
+inline void set_bit(int & n, int b) { n |= two(b); }
+inline void unset_bit(int & n, int b) { n &= ~two(b); }
+inline int last_bit(int n) { return n & (-n); }
+inline int ones(int n) { int res = 0; while(n && ++res) n-=n&(-n); return res; }
+
+template<class T> void chmax(T & a, const T & b) { a = max(a, b); }
+template<class T> void chmin(T & a, const T & b) { a = min(a, b); }
+
+
+class Solution {
+public:
+    priority_queue<int> maxHeap;
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+
+    void insertHeap(int x) {
+        if (maxHeap.size() == minHeap.size()) {
+            if (!minHeap.empty() && x > minHeap.top()) {
+                maxHeap.push(minHeap.top());
+                minHeap.pop();
+                minHeap.push(x);
+            } else {
+                maxHeap.push(x);
+            }
+        } else {
+            if (x < maxHeap.top()) {
+                minHeap.push(maxHeap.top());
+                maxHeap.pop();
+                maxHeap.push(x);
+            } else {
+                minHeap.push(x);
+            }
+        }
+    }
+
+    void balanceHeaps() {
+    }
+
+    double getMedian() {
+        if (maxHeap.size() == minHeap.size()) {
+            return (maxHeap.top() + minHeap.top()) / 2.0;
+        } else {
+            return maxHeap.top();
+        }
+    }
+};
+
+
+
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int n, x;
+    int t;
+    cin>>t;
+    while(t--)
+    {
+    	Solution ob;
+    	cin >> n;
+    	for(int i = 1;i<= n; ++i)
+    	{
+    		cin >> x;
+    		ob.insertHeap(x);
+    	    cout << floor(ob.getMedian()) << endl;
+    	}
+    
+cout << "~" << "\n";
+}
+	return 0;
+}
+// } Driver Code Ends
