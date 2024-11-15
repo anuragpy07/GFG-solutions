d1f5d80def5e64f3273eb70cbeffa82bae0c9f76
Anurag Kumar <anuragpandey93345@gmail.com>
2024-11-15 09:19:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Second Largest/second-largest.cpp
@@ -0,0 +1,92 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+#include <sstream>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <cctype>
+#include <string>
+#include <vector>
+#include <list>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <functional>
+using namespace std;
+
+#define DEBUG(x) cout << '>' << #x << ':' << x << endl;
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define FORD(i,a,b) for(int i=(a);i>=(b);i--)
+inline bool EQ(double a, double b) { return fabs(a-b) < 1e-9; }
+
+const int INF = 1<<29;
+typedef long long ll;
+
+inline int two(int n) { return 1 << n; }
+inline int test(int n, int b) { return (n>>b)&1; }
+inline void set_bit(int & n, int b) { n |= two(b); }
+inline void unset_bit(int & n, int b) { n &= ~two(b); }
+inline int last_bit(int n) { return n & (-n); }
+inline int ones(int n) { int res = 0; while(n && ++res) n-=n&(-n); return res; }
+
+template<class T> void chmax(T & a, const T & b) { a = max(a, b); }
+template<class T> void chmin(T & a, const T & b) { a = min(a, b); }
+
+class Solution {
+public:
+    int getSecondLargest(vector<int> &arr) {
+        int n = arr.size();
+        int largest = arr[0];
+        int secondLargest = -1;
+        
+        int i = 1;
+        while (i < n) {
+            largest = (arr[i] > largest) ? arr[i] : largest;
+            i++;
+        }
+        
+        i = 0;
+        while (i < n) {
+            secondLargest = (arr[i] != largest && arr[i] > secondLargest) ? arr[i] : secondLargest;
+            i++;
+        }
+        
+        return (secondLargest == largest || secondLargest == -1) ? -1 : secondLargest;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution ob;
+        int ans = ob.getSecondLargest(arr);
+        cout << ans << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
