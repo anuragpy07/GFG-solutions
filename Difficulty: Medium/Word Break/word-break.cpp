ac3746708d0eb6773f17b64714ab6712f45f0e02
Anurag Kumar <anuragpandey93345@gmail.com>
2024-11-16 15:00:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Word Break/word-break.cpp
@@ -0,0 +1,95 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+// s : given string to search
+// dictionary : vector of available strings
+
+#include <iostream> 
+#include <sstream> 
+#include <cstdio> 
+#include <cmath> 
+#include <cstring> 
+#include <cctype> 
+#include <string> 
+#include <vector> 
+#include <list> 
+#include <set> 
+#include <map> 
+#include <queue> 
+#include <stack> 
+#include <algorithm> 
+#include <functional> 
+using namespace std;  
+
+#define DEBUG(x) cout << '>' << #x << ':' << x << endl; 
+#define REP(i,n) for(int i=0;i<(n);i++) 
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++) 
+#define FORD(i,a,b) for(int i=(a);i>=(b);i--) 
+inline bool EQ(double a, double b) { return fabs(a-b) < 1e-9; } 
+
+const int INF = 1<<29; 
+typedef long long ll; 
+
+inline int two(int n) { return 1 << n; } 
+inline int test(int n, int b) { return (n>>b)&1; } 
+inline void set_bit(int & n, int b) { n |= two(b); } 
+inline void unset_bit(int & n, int b) { n &= ~two(b); } 
+inline int last_bit(int n) { return n & (-n); } 
+inline int ones(int n) { int res = 0; while(n && ++res) n-=n&(-n); return res; } 
+
+template<class T> void chmax(T & a, const T & b) { a = max(a, b); } 
+template<class T> void chmin(T & a, const T & b) { a = min(a, b); } 
+
+class Solution {
+public:
+    int wordBreak(int n, string s, vector<string>& dictionary) {
+        unordered_set<string> dict(dictionary.begin(), dictionary.end());
+        int len = s.length();
+        vector<bool> dp(len + 1, false);
+        dp[0] = true;
+        int i = 1;
+        while (i <= len) {
+            int j = 0;
+            while (j < i) {
+                dp[i] = dp[j] && dict.find(s.substr(j, i - j)) != dict.end() ? true : dp[i];
+                j++;
+            }
+            i++;
+        }
+        return dp[len] ? 1 : 0;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<string> dictionary;
+        for(int i=0;i<n;i++){
+            string S;
+            cin>>S;
+            dictionary.push_back(S);
+        }
+        string s;
+        cin>>s;
+        Solution ob;
+        cout<<ob.wordBreak(n, s, dictionary)<<"\n";
+    
+cout << "~" << "\n";
+}
+}
+
+// } Driver Code Ends
