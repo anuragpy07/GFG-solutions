72db7b8e484cfe2d59e707e245a6435efefa4631
Anurag Kumar <anuragpandey93345@gmail.com>
2024-11-18 16:08:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.cpp
@@ -0,0 +1,95 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+#include <iostream>
+#include <sstream>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <cctype>
+#include <string>
+#include <vector>
+#include <list>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <functional>
+using namespace std;
+
+#define DEBUG(x) cout << '>' << #x << ':' << x << endl;
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define FORD(i,a,b) for(int i=(a);i>=(b);i--)
+inline bool EQ(double a, double b) { return fabs(a-b) < 1e-9; }
+
+const int INF = 1<<29;
+typedef long long ll;
+
+inline int two(int n) { return 1 << n; }
+inline int test(int n, int b) { return (n>>b)&1; }
+inline void set_bit(int & n, int b) { n |= two(b); }
+inline void unset_bit(int & n, int b) { n &= ~two(b); }
+inline int last_bit(int n) { return n & (-n); }
+inline int ones(int n) { int res = 0; while(n && ++res) n-=n&(-n); return res; }
+
+template<class T> void chmax(T & a, const T & b) { a = max(a, b); }
+template<class T> void chmin(T & a, const T & b) { a = min(a, b); }
+
+class Solution {
+public:
+    int celebrity(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int left = 0, right = n - 1;
+
+        while (left < right) {
+            (mat[left][right] == 1) ? left++ : right--;
+        }
+
+        int potentialCelebrity = left;
+        int i = 0;
+        while (i < n) {
+            if (i != potentialCelebrity) {
+                if (mat[potentialCelebrity][i] == 1 || mat[i][potentialCelebrity] == 0) {
+                    return -1;
+                }
+            }
+            i++;
+        }
+        return potentialCelebrity;
+    }
+};
+
+
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<vector<int> > M(n, vector<int>(n, 0));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cin >> M[i][j];
+            }
+        }
+        Solution ob;
+        cout << ob.celebrity(M) << endl;
+        cout << "~" << endl;
+    }
+}
+
+// } Driver Code Ends
