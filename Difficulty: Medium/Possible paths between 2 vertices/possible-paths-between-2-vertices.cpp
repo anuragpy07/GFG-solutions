8af78e670cd568cf3aee1dbd5c1ffdde8305d150
Anurag Kumar <anuragpandey93345@gmail.com>
2024-11-18 19:47:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Possible paths between 2 vertices/possible-paths-between-2-vertices.cpp
@@ -0,0 +1,92 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+#include <sstream> 
+#include <cstdio> 
+#include <cmath> 
+#include <cstring> 
+#include <cctype> 
+#include <string> 
+#include <vector> 
+#include <list> 
+#include <set> 
+#include <map> 
+#include <queue> 
+#include <stack> 
+#include <algorithm> 
+#include <functional> 
+using namespace std;  
+ 
+#define DEBUG(x) cout << '>' << #x << ':' << x << endl; 
+#define REP(i,n) for(int i=0;i<(n);i++) 
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++) 
+#define FORD(i,a,b) for(int i=(a);i>=(b);i--) 
+inline bool EQ(double a, double b) { return fabs(a-b) < 1e-9; } 
+ 
+const int INF = 1<<29; 
+typedef long long ll; 
+ 
+inline int two(int n) { return 1 << n; } 
+inline int test(int n, int b) { return (n>>b)&1; } 
+inline void set_bit(int & n, int b) { n |= two(b); } 
+inline void unset_bit(int & n, int b) { n &= ~two(b); } 
+inline int last_bit(int n) { return n & (-n); } 
+inline int ones(int n) { int res = 0; while(n && ++res) n-=n&(-n); return res; } 
+ 
+template<class T> void chmax(T & a, const T & b) { a = max(a, b); } 
+template<class T> void chmin(T & a, const T & b) { a = min(a, b); }
+class Solution {
+public:
+    void dfs(int node, vector<int> adj[], vector<bool>& visited, int destination, int& pathCount) {
+        if (node == destination) {
+            pathCount++;
+            return;
+        }
+        
+        visited[node] = true;
+        
+        for (int neighbor : adj[node]) {
+            if (!visited[neighbor]) {
+                dfs(neighbor, adj, visited, destination, pathCount);
+            }
+        }
+        
+        visited[node] = false;
+    }
+    
+    int countPaths(int V, vector<int> adj[], int source, int destination) {
+        vector<bool> visited(V, false);
+        int pathCount = 0;
+        dfs(source, adj, visited, destination, pathCount);
+        return pathCount;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >> E;
+        vector<int> adj[V];
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            // adj[v].push_back(u);
+        }
+        int source, destination;
+        cin >> source >> destination;
+        Solution obj;
+        cout << obj.countPaths(V, adj, source, destination) << endl;
+    
+cout << "~" << "\n";
+}
+    return 0;
+}
+// } Driver Code Ends
