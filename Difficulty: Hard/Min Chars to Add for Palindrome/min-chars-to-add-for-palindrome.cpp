bdca119c9cf4439e34353ab8acd5f9d3fdd87b30
Anurag Kumar <anuragpandey93345@gmail.com>
2024-11-21 03:43:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Min Chars to Add for Palindrome/min-chars-to-add-for-palindrome.cpp
@@ -0,0 +1,93 @@
+//{ Driver Code Starts
+/* Driver program to test above function */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+#include <iostream>
+#include <sstream>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <cctype>
+#include <string>
+#include <vector>
+#include <list>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <functional>
+using namespace std;
+
+#define DEBUG(x) cout << '>' << #x << ':' << x << endl;
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define FORD(i,a,b) for(int i=(a);i>=(b);i--)
+inline bool EQ(double a, double b) { return fabs(a-b) < 1e-9; }
+
+const int INF = 1<<29;
+typedef long long ll;
+
+inline int two(int n) { return 1 << n; }
+inline int test(int n, int b) { return (n>>b)&1; }
+inline void set_bit(int & n, int b) { n |= two(b); }
+inline void unset_bit(int & n, int b) { n &= ~two(b); }
+inline int last_bit(int n) { return n & (-n); }
+inline int ones(int n) { int res = 0; while(n && ++res) n-=n&(-n); return res; }
+
+template<class T> void chmax(T & a, const T & b) { a = max(a, b); }
+template<class T> void chmin(T & a, const T & b) { a = min(a, b); }
+
+class Solution {
+public:
+    int minChar(string s) {
+        if (s.length() <= 1) return 0;
+        string rev = s;
+        reverse(rev.begin(), rev.end());
+        string concat = s + "#" + rev;
+        vector<int> lps(concat.length(), 0);
+        int len = 0, i = 1;
+        while (i < concat.length()) {
+            if (concat[i] == concat[len]) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        return s.length() - lps[concat.length() - 1];
+    }
+};
+
+
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string str;
+        cin >> str;
+        Solution ob;
+        int ans = ob.minChar(str);
+        cout << ans << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
