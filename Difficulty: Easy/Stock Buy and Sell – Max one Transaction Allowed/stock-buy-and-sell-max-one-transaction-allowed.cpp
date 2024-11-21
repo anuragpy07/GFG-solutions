361e036a6524a63e5150e2037e73010fd5224f80
Anurag Kumar <anuragpandey93345@gmail.com>
2024-11-22 00:26:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Stock Buy and Sell – Max one Transaction Allowed/stock-buy-and-sell-max-one-transaction-allowed.cpp
@@ -0,0 +1,86 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+#include <iostream> 
+#include <sstream> 
+#include <cstdio> 
+#include <cmath> 
+#include <cstring> 
+#include <cctype> 
+#include <string> 
+#include <vector> 
+#include <list> 
+#include <set> 
+#include <map> 
+#include <queue> 
+#include <stack> 
+#include <algorithm> 
+#include <functional> 
+using namespace std;  
+ 
+#define DEBUG(x) cout << '>' << #x << ':' << x << endl; 
+#define REP(i,n) for(int i=0;i<(n);i++) 
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++) 
+#define FORD(i,a,b) for(int i=(a);i>=(b);i--) 
+inline bool EQ(double a, double b) { return fabs(a-b) < 1e-9; } 
+ 
+const int INF = 1<<29; 
+typedef long long ll; 
+ 
+inline int two(int n) { return 1 << n; } 
+inline int test(int n, int b) { return (n>>b)&1; } 
+inline void set_bit(int & n, int b) { n |= two(b); } 
+inline void unset_bit(int & n, int b) { n &= ~two(b); } 
+inline int last_bit(int n) { return n & (-n); } 
+inline int ones(int n) { int res = 0; while(n && ++res) n-=n&(-n); return res; } 
+ 
+template<class T> void chmax(T & a, const T & b) { a = max(a, b); } 
+template<class T> void chmin(T & a, const T & b) { a = min(a, b); } 
+
+class Solution {
+public:
+    int maximumProfit(vector<int>& prices) {
+        int minimumPrice = INT_MAX;
+        int maximumProfit = 0;
+        int currentIndex = 0;
+        
+        while(currentIndex < prices.size()) {
+            minimumPrice = (prices[currentIndex] < minimumPrice) ? prices[currentIndex] : minimumPrice;
+            maximumProfit = (prices[currentIndex] - minimumPrice > maximumProfit) ? prices[currentIndex] - minimumPrice : maximumProfit;
+            currentIndex++;
+        }
+        
+        return maximumProfit;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> prices;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            prices.push_back(number);
+        }
+
+        Solution ob;
+        int ans = ob.maximumProfit(prices);
+        cout << ans << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
