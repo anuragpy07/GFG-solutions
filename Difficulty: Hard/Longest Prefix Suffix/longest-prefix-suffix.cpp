917071f910b0a6196a9034c7e1b07f308b551a32
Anurag Kumar <anuragpandey93345@gmail.com>
2024-11-21 23:03:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Longest Prefix Suffix/longest-prefix-suffix.cpp
@@ -0,0 +1,102 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+// User function template for C++
+
+#include <iostream> 
+#include <sstream> 
+#include <cstdio> 
+#include <cmath> 
+#include <cstring> 
+#include <cctype> 
+#include <string> 
+#include <vector> 
+#include <list> 
+#include <set> 
+#include <map> 
+#include <queue> 
+#include <stack> 
+#include <algorithm> 
+#include <functional> 
+using namespace std;  
+ 
+#define DEBUG(x) cout << '>' << #x << ':' << x << endl; 
+#define REP(i,n) for(int i=0;i<(n);i++) 
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++) 
+#define FORD(i,a,b) for(int i=(a);i>=(b);i--) 
+inline bool EQ(double a, double b) { return fabs(a-b) < 1e-9; } 
+ 
+const int INF = 1<<29; 
+typedef long long ll; 
+ 
+inline int two(int n) { return 1 << n; } 
+inline int test(int n, int b) { return (n>>b)&1; } 
+inline void set_bit(int & n, int b) { n |= two(b); } 
+inline void unset_bit(int & n, int b) { n &= ~two(b); } 
+inline int last_bit(int n) { return n & (-n); } 
+inline int ones(int n) { int res = 0; while(n && ++res) n-=n&(-n); return res; } 
+ 
+template<class T> void chmax(T & a, const T & b) { a = max(a, b); } 
+template<class T> void chmin(T & a, const T & b) { a = min(a, b); } 
+
+class Solution {
+public:
+    int longestPrefixSuffix(string &s) {
+        int n = s.length();
+        int len = 0;
+        
+        vector<int> lps(n, 0);
+        int i = 1;
+        
+        while (i < n) {
+            if (s[i] == s[len]) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        
+        return lps[n-1];
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        string str;
+        cin >> str;
+
+        Solution ob;
+
+        cout << ob.longestPrefixSuffix(str) << "\n";
+
+        cout << "~"
+             << "\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
