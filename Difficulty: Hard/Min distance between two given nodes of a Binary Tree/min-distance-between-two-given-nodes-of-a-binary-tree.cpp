fbf768562aa3607962fee6fedd21a6104795d620
Anurag Kumar <anuragpandey93345@gmail.com>
2024-11-22 22:11:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Min distance between two given nodes of a Binary Tree/min-distance-between-two-given-nodes-of-a-binary-tree.cpp
@@ -0,0 +1,180 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_HEIGHT 100000
+
+// Tree Node
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val) {
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+
+// Function to Build Tree
+Node* buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N') return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;) ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size()) break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+/* A binary tree node
+struct Node
+{
+    int data;
+    Node* left, * right;
+}; */
+
+#include <iostream>
+#include <sstream>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <cctype>
+#include <string>
+#include <vector>
+#include <list>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <functional>
+using namespace std;
+
+
+class Solution {
+public:
+    Node* findLCA(Node* root, int a, int b) {
+        while(root == NULL) return NULL;
+        
+        while(root->data == a || root->data == b) return root;
+        
+        Node* left = findLCA(root->left, a, b);
+        Node* right = findLCA(root->right, a, b);
+        
+        while(left && right) return root;
+        
+        return (left != NULL) ? left : right;
+    }
+    
+    int findLevel(Node* root, int val, int level) {
+        while(root == NULL) return -1;
+        
+        while(root->data == val) return level;
+        
+        int left = findLevel(root->left, val, level + 1);
+        while(left == -1) {
+            left = findLevel(root->right, val, level + 1);
+            return left;
+        }
+        
+        return left;
+    }
+    
+    int findDist(Node* root, int a, int b) {
+        Node* lca = findLCA(root, a, b);
+        
+        int dist1 = findLevel(lca, a, 0);
+        int dist2 = findLevel(lca, b, 0);
+        
+        return (dist1 == -1 || dist2 == -1) ? -1 : (dist1 + dist2);
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+/* Driver program to test size function*/
+
+  
+
+int main() {
+
+   
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+        string s, ch;
+        getline(cin, s);
+        
+        Node* root = buildTree(s);
+        int n1, n2;
+        scanf("%d %d ", &n1, &n2);
+        Solution ob;
+        cout << ob.findDist(root, n1, n2) << endl;
+     
+    }
+    return 0;
+}
+
+// } Driver Code Ends
