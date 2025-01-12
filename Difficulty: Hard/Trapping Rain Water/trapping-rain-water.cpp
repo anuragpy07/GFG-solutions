aaf57a95e9e8920ea5d71ec91030a9e4a2847cd0
Anurag Kumar <anuragpandey93345@gmail.com>
2025-01-12 12:18:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Trapping Rain Water/trapping-rain-water.cpp
+++ Difficulty: Hard/Trapping Rain Water/trapping-rain-water.cpp
@@ -6,63 +6,36 @@ using namespace std;
 
 // } Driver Code Ends
 
-#include <iostream>
-#include <sstream>
-#include <cstdio>
-#include <cmath>
-#include <cstring>
-#include <cctype>
-#include <string>
-#include <vector>
-#include <list>
-#include <set>
-#include <map>
-#include <queue>
-#include <stack>
-#include <algorithm>
-#include <functional>
-using namespace std;
-
-#define DEBUG(x) cout << '>' << #x << ':' << x << endl;
-#define REP(i,n) for(int i=0;i<(n);i++)
-#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
-#define FORD(i,a,b) for(int i=(a);i>=(b);i--)
-inline bool EQ(double a, double b) { return fabs(a-b) < 1e-9; }
-
-const int INF = 1<<29;
-typedef long long ll;
-
-inline int two(int n) { return 1 << n; }
-inline int test(int n, int b) { return (n>>b)&1; }
-inline void set_bit(int & n, int b) { n |= two(b); }
-inline void unset_bit(int & n, int b) { n &= ~two(b); }
-inline int last_bit(int n) { return n & (-n); }
-inline int ones(int n) { int res = 0; while(n && ++res) n-=n&(-n); return res; }
-
-template<class T> void chmax(T & a, const T & b) { a = max(a, b); }
-template<class T> void chmin(T & a, const T & b) { a = min(a, b); }
-
 class Solution {
 public:
-    int trappingWater(vector<int>& arr) {
-        int left = 0, right = arr.size() - 1;
-        int leftMax = 0, rightMax = 0;
-        int result = 0;
-        
-        while (left < right) {
-            arr[left] <= arr[right] ? 
-                (leftMax = max(leftMax, arr[left]),
-                 result += leftMax - arr[left],
-                 left++)
-                :
-                (rightMax = max(rightMax, arr[right]),
-                 result += rightMax - arr[right],
-                 right--);
+    int maxWater(vector<int> &arr) {
+        int n = arr.size();
+        if (n <= 1) return 0;
+
+        vector<int> leftMax(n), rightMax(n);
+        leftMax[0] = arr[0];
+        rightMax[n - 1] = arr[n - 1];
+
+      
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = max(leftMax[i - 1], arr[i]);
         }
-        
-        return result;
+
+     
+        for (int i = n - 2; i >= 0; i--) {
+            rightMax[i] = max(rightMax[i + 1], arr[i]);
+        }
+
+       
+        int waterTrapped = 0;
+        for (int i = 0; i < n; i++) {
+            waterTrapped += min(leftMax[i], rightMax[i]) - arr[i];
+        }
+
+        return waterTrapped;
     }
 };
+
 
 
 //{ Driver Code Starts.
@@ -83,7 +56,7 @@ int main() {
         }
 
         Solution ob;
-        int res = ob.trappingWater(arr);
+        int res = ob.maxWater(arr);
 
         cout << res << endl << "~" << endl;
     }
