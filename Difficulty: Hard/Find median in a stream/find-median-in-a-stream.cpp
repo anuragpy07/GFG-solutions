b23288531c0fca4a8274d84cafb5116bf7f22e13
Anurag Kumar <anuragpandey93345@gmail.com>
2025-02-20 14:16:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Find median in a stream/find-median-in-a-stream.cpp
+++ Difficulty: Hard/Find median in a stream/find-median-in-a-stream.cpp
@@ -3,107 +3,75 @@
 using namespace std;
 
 
-
 // } Driver Code Ends
 
-#include <iostream>
-#include <sstream>
-#include <cstdio>
-#include <cmath>
-#include <cstring>
-#include <cctype>
-#include <string>
-#include <vector>
-#include <list>
-#include <set>
-#include <map>
-#include <queue>
-#include <stack>
-#include <algorithm>
-#include <functional>
-using namespace std;
-
-#define DEBUG(x) cout << '>' << #x << ':' << x << endl;
-#define REP(i,n) for(int i=0;i<(n);i++)
-#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
-#define FORD(i,a,b) for(int i=(a);i>=(b);i--)
-inline bool EQ(double a, double b) { return fabs(a-b) < 1e-9; }
-
-const int INF = 1<<29;
-typedef long long ll;
-
-inline int two(int n) { return 1 << n; }
-inline int test(int n, int b) { return (n>>b)&1; }
-inline void set_bit(int & n, int b) { n |= two(b); }
-inline void unset_bit(int & n, int b) { n &= ~two(b); }
-inline int last_bit(int n) { return n & (-n); }
-inline int ones(int n) { int res = 0; while(n && ++res) n-=n&(-n); return res; }
-
-template<class T> void chmax(T & a, const T & b) { a = max(a, b); }
-template<class T> void chmin(T & a, const T & b) { a = min(a, b); }
-
-
 class Solution {
-public:
+    // Max heap to store the lower half of numbers
     priority_queue<int> maxHeap;
+    // Min heap to store the upper half of numbers
     priority_queue<int, vector<int>, greater<int>> minHeap;
-
-    void insertHeap(int x) {
-        if (maxHeap.size() == minHeap.size()) {
-            if (!minHeap.empty() && x > minHeap.top()) {
-                maxHeap.push(minHeap.top());
-                minHeap.pop();
-                minHeap.push(x);
+    
+public:
+    vector<double> getMedian(vector<int> &arr) {
+        vector<double> medians;
+        
+        for(int num : arr) {
+            // Step 1: Add new element
+            if(maxHeap.empty() || num < maxHeap.top()) {
+                maxHeap.push(num);
             } else {
-                maxHeap.push(x);
+                minHeap.push(num);
             }
-        } else {
-            if (x < maxHeap.top()) {
+            
+            // Step 2: Balance heaps
+            // Max heap can have at most one more element than min heap
+            if(maxHeap.size() > minHeap.size() + 1) {
                 minHeap.push(maxHeap.top());
                 maxHeap.pop();
-                maxHeap.push(x);
+            } else if(minHeap.size() > maxHeap.size()) {
+                maxHeap.push(minHeap.top());
+                minHeap.pop();
+            }
+            
+            // Step 3: Calculate median
+            if(maxHeap.size() > minHeap.size()) {
+                // Odd number of elements
+                medians.push_back(maxHeap.top());
             } else {
-                minHeap.push(x);
+                // Even number of elements
+                double median = (maxHeap.top() + minHeap.top()) / 2.0;
+                medians.push_back(median);
             }
         }
-    }
-
-    void balanceHeaps() {
-    }
-
-    double getMedian() {
-        if (maxHeap.size() == minHeap.size()) {
-            return (maxHeap.top() + minHeap.top()) / 2.0;
-        } else {
-            return maxHeap.top();
-        }
+        
+        return medians;
     }
 };
 
 
-
-
-
 //{ Driver Code Starts.
-
-int main()
-{
-    int n, x;
+int main() {
     int t;
-    cin>>t;
-    while(t--)
-    {
-    	Solution ob;
-    	cin >> n;
-    	for(int i = 1;i<= n; ++i)
-    	{
-    		cin >> x;
-    		ob.insertHeap(x);
-    	    cout << floor(ob.getMedian()) << endl;
-    	}
-    
-cout << "~" << "\n";
-}
-	return 0;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+
+        string s;
+        getline(cin, s);
+        stringstream ss(s);
+        vector<int> nums;
+        int num;
+        while (ss >> num) {
+            nums.push_back(num);
+        }
+        Solution ob;
+        vector<double> ans = ob.getMedian(nums);
+        cout << fixed << setprecision(1);
+        for (auto &i : ans)
+            cout << i << " ";
+        cout << "\n";
+        cout << "~" << endl;
+    }
+    return 0;
 }
 // } Driver Code Ends
